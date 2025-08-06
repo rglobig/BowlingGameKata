284e28eb9aac6cf8e2cc9f1cac781b3c26dd8dd5
rene <rene>
2025-08-06 20:47:49 +0200

    style

--- Code/Round.cs
+++ Code/Round.cs
@@ -4,6 +4,12 @@ internal record StrikeRound() : Round(new StrikeRoll(), new ZeroRoll());
 
 internal record Round
 {
+    public Roll First { get; }
+    public Roll Second { get; }
+    public int PinsHit => First.PinsHit + Second.PinsHit;
+    public bool IsSpare => PinsHit == Game.MaxPins && First.PinsHit < Game.MaxPins;
+    public bool IsStrike => First.PinsHit == Game.MaxPins;
+
     public Round(Roll first, Roll second)
     {
         First = first;
@@ -12,10 +18,4 @@ internal record Round
         if (PinsHit > Game.MaxPins)
             throw new ArgumentException($"Sum of first and second throws cannot be greater than {Game.MaxPins}");
     }
-
-    public Roll First { get; }
-    public Roll Second { get; }
-    public int PinsHit => First.PinsHit + Second.PinsHit;
-    public bool IsSpare => PinsHit == Game.MaxPins && First.PinsHit < Game.MaxPins;
-    public bool IsStrike => First.PinsHit == Game.MaxPins;
 }
